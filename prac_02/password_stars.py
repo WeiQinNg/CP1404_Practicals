a9bf7d9b70275a77337f84f03af5d905d5fc758d
feistyhippo <feistyhippo98@gmail.com>
2023-07-17 15:06:45 +0800

    Add docstring to each function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,15 +1,18 @@
 MINIMUM_LENGTH = 5
 
 def main():
+	"""Password checking program"""
 	password = get_password()
 	print_asterisks(password)
 
 
 def print_asterisks(password):
+	"""Print asterisks based on length of password"""
 	print(len(password) * "*")
 
 
 def get_password():
+	"""Get password and check length"""
 	password = input("Enter password: ")
 	while len(password) < MINIMUM_LENGTH:
 		print("Use a longer password")
