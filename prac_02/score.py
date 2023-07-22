21c5b6bab19d42a2efec311b07dc99fa8bb7beca
feistyhippo <feistyhippo98@gmail.com>
2023-07-22 13:15:00 +0800

    Refactor score status determining program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -4,13 +4,24 @@ Broken program to determine score status
 """
 
 # TODO: Fix this!
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Score status determining program."""
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+    score = random.randint(0, 100)
+    print(determine_status(score))
+
+def determine_status(score):
+    """Determine score status using a given score."""
+    if score < 0 or score > 100:
+        return("Invalid score")
+    elif score >= 90:
+        return("Excellent")
+    elif score >= 50:
+        return("Passable")
+    else:
+        return("Bad")
+
+main()
