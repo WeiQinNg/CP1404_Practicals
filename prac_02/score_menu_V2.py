4386e600696d90cdb3b8a67e71f5d7683f1c2b43
feistyhippo <feistyhippo98@gmail.com>
2023-07-24 15:27:47 +0800

    Completed the two TODO exercises

---
+++ prac_02/score_menu_V2.py
@@ -0,0 +1,57 @@
+"""
+CP1404 - Practical 2
+"""
+MENU = "(G)et a valid score", "(P)rint result", "(S)how stars", "(Q)uit"
+
+def main():
+    """Program with menu that takes in a given score to ."""
+    score = ""
+    print(*MENU, sep="\n")
+    menu_choice = input(">>> ").upper()
+    while menu_choice != "Q":
+        if menu_choice == "G":
+            score = get_score()
+        elif menu_choice == "P":
+            if score == "":
+                score = print_error(score)
+            else:
+                print(determine_status(score))
+        elif menu_choice == "S":
+            if score == "":
+                score = print_error(score)
+            else:
+                print(score * "*")
+        else:
+            print("Invalid menu choice")
+        print(*MENU, sep="\n")
+        menu_choice = input(">>> ").upper()
+    print("Farewell")
+
+
+def print_error(score):
+    """Print error message and obtain a score."""
+    print("Please enter a score first")
+    score = get_score()
+    return score
+
+
+def get_score():
+    """Get a valid score."""
+    score = int(input("Enter score: "))
+    if score < 0 or score > 100:
+        print("Invalid score")
+        score = int(input("Enter score: "))
+    return score
+
+
+def determine_status(score):
+    """Determine score status using a given score."""
+    if score >= 90:
+        return("Excellent")
+    elif score >= 50:
+        return("Passable")
+    else:
+        return("Bad")
+
+
+main()
