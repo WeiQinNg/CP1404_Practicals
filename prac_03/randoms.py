fb4cfe81a301714c9e94f21ef38d8f62fc3bfbde
feistyhippo <feistyhippo98@gmail.com>
2023-07-30 22:59:29 +0800

    Edited randoms exercise

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,5 +1,19 @@
+"""
+CP1404/CP5632 - Practical 3
+"""
 import random
 
 print(random.randint(5, 20))  # line 1
+# A single integer was printed
+# Smallest is 5, largest is 20
+
 print(random.randrange(3, 10, 2))  # line 2
+# A single integer was printed
+# Smallest is 3, largest is 9
+# No
+
 print(random.uniform(2.5, 5.5))  # line 3
+# A single float number was printed
+# Smallest is 2.5, largest is 5.5
+
+print(random.randint(1, 100))
