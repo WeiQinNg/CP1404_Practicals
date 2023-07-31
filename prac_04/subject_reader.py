0f33ab7286f9b5f82438f18808c499c467330ce1
feistyhippo <feistyhippo98@gmail.com>
2023-07-31 15:45:52 +0800

    Modify get_data function to return a nested list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -13,6 +13,7 @@ def main():
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    lists = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -23,7 +24,8 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        lists.append(parts)
     input_file.close()
-
+    return lists
 
 main()
