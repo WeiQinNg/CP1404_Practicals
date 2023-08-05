1951fd687055eedb9d515a3bc00d7e2cab37453c
feistyhippo <feistyhippo98@gmail.com>
2023-08-05 14:50:38 +0800

    Refactor program into function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,13 +7,24 @@ import random
 MINIMUM_VALUE = 1
 MAXIMUM_VALUE = 45
 NUMBER_OF_VALUES = 6
+MINIMUM_NUMBER = 1
 
-number_of_picks = int(input("How many quick picks? "))
-for i in range(number_of_picks):
-    numbers = []
-    while len(numbers) < NUMBER_OF_VALUES:
-        number = random.randint(MINIMUM_VALUE, MAXIMUM_VALUE)
-        if number not in numbers:
-            numbers.append(number)
-    numbers.sort()
-    print(*[f"{number:>2}" for number in numbers], sep=" ")
+
+def main():
+    """Generate a specified number of quick picks."""
+    number_of_picks = int(input("How many quick picks? "))
+    while number_of_picks < MINIMUM_NUMBER:
+        print("Number of picks must be more than 0")
+        number_of_picks = int(input("How many quick picks? "))
+
+    for i in range(number_of_picks):
+        numbers = []
+        while len(numbers) < NUMBER_OF_VALUES:
+            number = random.randint(MINIMUM_VALUE, MAXIMUM_VALUE)
+            if number not in numbers:
+                numbers.append(number)
+        numbers.sort()
+        print(*[f"{number:>2}" for number in numbers], sep=" ")
+
+
+main()
