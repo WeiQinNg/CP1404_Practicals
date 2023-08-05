f03d1213ee4bdecdcd1aca67a78c1b8fd7758124
feistyhippo <feistyhippo98@gmail.com>
2023-08-05 14:15:12 +0800

    Changed print_report function to use length of incomes to represent number of months and printed statement from using string concatenation to f-string

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -13,17 +13,17 @@ def main():
         income = float(input(f"Enter income for month {str(month)}: "))
         incomes.append(income)
 
-    print_report(incomes, number_of_months)
+    print_report(incomes)
 
 
-def print_report(incomes, number_of_months):
+def print_report(incomes):
     """Print income report."""
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, number_of_months + 1):
+    for month in range(1, len(incomes) + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f} Total: ${total:10.2f}")
 
 
 main()
