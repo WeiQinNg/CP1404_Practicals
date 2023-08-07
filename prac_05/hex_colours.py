55fb821857f198ae36879b45b5cd7dab96fa22d3
feistyhippo <feistyhippo98@gmail.com>
2023-08-07 14:28:10 +0800

    Add hex colours program

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,7 @@
+"""
+CP1404/CP5632 Practical 5
+hex colours in a dictionary
+"""
+CODE_TO_NAME = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "	#f0f8ff",
+                "Alizarin crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc",
+                "AntiqueWhite": "#faebd7", "Apricot": "#fbceb1", "Aqua": "#00ffff"}
