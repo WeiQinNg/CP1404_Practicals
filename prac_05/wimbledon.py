39ed9df16879e04b1896601093c309fb9df6057b
feistyhippo <feistyhippo98@gmail.com>
2023-08-13 23:56:33 +0800

    Refactor processing records into function

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -6,13 +6,25 @@ Estimate: 25 minutes
 Actual: 60 minutes
 """
 FILENAME = "wimbledon.csv"
-
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
 
 def main():
     """Read data file and display Wimbledon champions details."""
     records = get_records(FILENAME)
+    champion_to_count, countries = process_records(records)
 
 
+def process_records(records):
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[COUNTRY_INDEX])
+        try:
+            champion_to_count[record[CHAMPION_INDEX]] += 1
+        except KeyError:
+            champion_to_count[record[CHAMPION_INDEX]] = 1
+    return champion_to_count, countries
 
 
 def get_records(filename):
