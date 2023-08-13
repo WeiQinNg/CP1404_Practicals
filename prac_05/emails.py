447d7674e3b494c1d7c1371b1b76be3ffa82b6d0
feistyhippo <feistyhippo98@gmail.com>
2023-08-13 14:45:14 +0800

    Refactor name extraction to use function

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -8,10 +8,17 @@ Actual:  minutes
 
 
 def main():
+    """Program to store emails and names in a dictionary."""
     while user_email != "":
         user_email = input("Email: ")
-        user_name = user_email.split('@')[0].title()
+        user_name = extract_name(user_email)
         print(f"Is your name {user_name}? (Y/n)")
+
+
+def extract_name(user_email):
+    """Extract name from user email."""
+    user_name = user_email.split('@')[0].title()
+    return user_name
 
 
 main()
