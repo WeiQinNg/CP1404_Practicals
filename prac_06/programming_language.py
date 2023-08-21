1448b97ae77ee768a78ba5d1f66e0b34ec109fda
feistyhippo <feistyhippo98@gmail.com>
2023-08-21 15:19:52 +0800

    Add programming language class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,14 @@
+"""CP1404/CP5632 Practical 6 - Programming language class."""
+
+class ProgrammingLanguage:
+    """Programming languages."""
+
+    def __init__(self, name="", typing="", reflection="", year=0):
+        """Initialise a programming language.
+
+
+        """
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
