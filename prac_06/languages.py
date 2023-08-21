717c642e7d844d7e14cbbd4a2d267674df9b8e24
feistyhippo <feistyhippo98@gmail.com>
2023-08-21 15:35:12 +0800

    Add programming language client

---
+++ prac_06/languages.py
@@ -0,0 +1,13 @@
+"""CP1404/CP5632 Practical 6 - Programming language client."""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+
+main()
