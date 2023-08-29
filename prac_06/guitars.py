59699bfef333cd43b360dac840971f16836ef892
feistyhippo <feistyhippo98@gmail.com>
2023-08-30 00:08:54 +0800

    Add user input

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,2 +1,16 @@
 """CP1404/CP5632 Practical 6 - Guitars program."""
 
+from prac_06.guitar import Guitar
+
+WELCOME_MESSAGE = "My guitars!"
+
+
+def main():
+    print(WELCOME_MESSAGE)
+    guitars = []
+    name = input("Name: ")
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    guitars.append([name, year, cost])
+    print(f"{name} ({year}) : ${cost} added.")
+
