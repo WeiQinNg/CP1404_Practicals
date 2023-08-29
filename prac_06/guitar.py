06bebd7af8118f939f2b3046c45e671ef986c2c2
feistyhippo <feistyhippo98@gmail.com>
2023-08-29 14:11:07 +0800

    Add init method

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,5 +1,17 @@
 """CP1404/CP5632 Practical 6 - Guitar class."""
 
+
 class Guitar:
     """Guitars."""
 
+    def __init__(self, name="", year=0, cost=0.0):
+        """Initialise a guitar.
+
+        name: string, reference to guitar name
+        year: integer, reference to the year in which the guitar was made
+        cost: float, reference to the cost of the guitar
+        """
+
+        self.name = name
+        self.year = year
+        self.cost = cost
