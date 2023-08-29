b554663f16377e07a197b7a69bfef963d5c84a96
feistyhippo <feistyhippo98@gmail.com>
2023-08-29 13:33:49 +0800

    Add loop to print dynamic languages

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -9,5 +9,12 @@ def main():
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
     print(python)
 
+    languages = [python, ruby, visual_basic]
+    print(languages)
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
 
 main()
