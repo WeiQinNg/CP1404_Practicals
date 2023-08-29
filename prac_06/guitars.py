b0cb3c603e1030ed2f091950942aad0a73eeed38
feistyhippo <feistyhippo98@gmail.com>
2023-08-30 00:27:42 +0800

    Add printing of details of each guitar

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -7,10 +7,21 @@ WELCOME_MESSAGE = "My guitars!"
 
 def main():
     print(WELCOME_MESSAGE)
+    # name = input("Name: ")
     guitars = []
-    name = input("Name: ")
-    year = int(input("Year: "))
-    cost = float(input("Cost: $"))
-    guitars.append([name, year, cost])
-    print(f"{name} ({year}) : ${cost} added.")
+    # while name != "":
+        # year = int(input("Year: "))
+        # cost = float(input("Cost: $"))
+        # guitars.append(Guitar(name, year, cost))
+        # print(f"{name} ({year}) : ${cost} added.")
 
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
+
+
+main()
