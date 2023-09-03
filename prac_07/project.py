b5456c5983fb277390fb6be8d6b5943916f496f9
feistyhippo <feistyhippo98@gmail.com>
2023-09-03 15:18:55 +0800

    Add project class

---
+++ prac_07/project.py
@@ -0,0 +1,10 @@
+"""CP1404/CP5632 Practical 07
+Project class
+Start time: 03 Sept 2023 1515
+End time: 03 Sept 2023
+"""
+
+class Project:
+    """Represent information about a project."""
+
+    def __init__(self):
