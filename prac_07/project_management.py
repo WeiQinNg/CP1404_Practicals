4511263338c634e7de78bada32e4afa192c0cb6b
feistyhippo <feistyhippo98@gmail.com>
2023-09-03 15:24:26 +0800

    Add menu statement

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -4,3 +4,4 @@ Start time: 03 Sept 2023 1515
 End time: 03 Sept 2023
 """
 
+MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n- (U)pdate project\n- (Q)uit"
