b2aac6ae8465312350822a863662a244f3ff5d69
feistyhippo <feistyhippo98@gmail.com>
2023-09-03 13:41:08 +0800

    Add user input for new guitar, save guitars to csv file

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -19,5 +19,19 @@ def main():
     for guitar in guitars:
         print(guitar)
 
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(new_guitar, "added.")
+        name = input("Name: ")
+
+    out_file = open(FILENAME, 'w', encoding="utf-8-sig")
+    for guitar in guitars:
+        print(f"{guitar.name}, {guitar.year}, {guitar.cost}", file=out_file)
+    out_file.close()
+
 
 main()
