4b6b7ad65c090ed39767b948811a0ec0555138b7
feistyhippo <feistyhippo98@gmail.com>
2023-09-04 23:37:32 +0800

    Add class attributes using init

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,10 +1,17 @@
 """CP1404/CP5632 Practical 07
 Project class
 Start time: 03 Sept 2023 1515
-End time: 03 Sept 2023
+End time: 04 Sept 2023
 """
+import datetime
+
 
 class Project:
     """Represent information about a project."""
 
-    def __init__(self):
+    def __init__(self, name="", start_date="", priority=int, cost=float, completion=int):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost = cost
+        self.completion = completion
