75cc978a61c695096359789ec2e44741c3ef799d
feistyhippo <feistyhippo98@gmail.com>
2023-09-05 13:45:46 +0800

    Add str method

--- prac_07/project.py
+++ prac_07/project.py
@@ -10,8 +10,13 @@ class Project:
     """Represent information about a project."""
 
     def __init__(self, name="", start_date="", priority=int, cost=float, completion=int):
+        """Construct a line based on the information of a project."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
         self.cost = cost
         self.completion = completion
+
+    def __str__(self):
+        """Return string representation of a project."""
+        return f"{self.name}, {self.start_date}, {self.priority}, {self.cost:,.2f}, {self.completion}"
