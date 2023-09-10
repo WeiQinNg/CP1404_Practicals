9408b0de1e15e7bf40ff971428aa1a0ec10bb310
feistyhippo <feistyhippo98@gmail.com>
2023-09-10 22:49:39 +0800

    Edited init method

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -10,13 +10,14 @@ from kivy.lang import Builder
 class DynamicLabelsApp(App):
     """Main program - Kivy app for dynamic label creation."""
 
-    def __init__(self, names):
+    def __init__(self):
         """Construct a name from a list of names."""
-        names = ["Alice", "Bob", "Charlie"]
-        self.names = names
+        self.names = ["Alice", "Bob", "Charlie"]
 
     def build(self):
         """Build the Kivy GUI."""
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
         self.create_widgets()
+
+    def create_widgets(self):
