5417a428a96a68ed85ac87957f7b3737274aa683
feistyhippo <feistyhippo98@gmail.com>
2023-09-10 23:12:20 +0800

    Edited create_widgets method to use Label instead of Button

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -5,14 +5,15 @@ Ng Wei Qin
 """
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.button import Button
+from kivy.uix.button import Label
 
 
 class DynamicLabelsApp(App):
     """Main program - Kivy app for dynamic label creation."""
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         """Construct a name from a list of names."""
+        super().__init__(**kwargs)
         self.names = ["Alice", "Bob", "Charlie"]
 
     def build(self):
@@ -20,11 +21,14 @@ class DynamicLabelsApp(App):
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
         self.create_widgets()
+        return self.root
 
     def create_widgets(self):
         """Create labels from data and add them to the GUI."""
         for name in self.names:
             # create a button for each data entry, specifying the text
-            temp_label = Button(text=name)
-            temp_label.bind(on_press=self.press_entry)
+            temp_label = Label(text=name)
             self.root.ids.main_labels.add_widget(temp_label)
+
+
+DynamicLabelsApp().run()
