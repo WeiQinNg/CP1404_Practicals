7f4c241d0f09a1cf88585655169b98137c91b470
feistyhippo <feistyhippo98@gmail.com>
2023-09-10 22:17:10 +0800

    Add dynamic labels program

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,11 @@
+"""
+CP1404/CP5632 Practical 08
+Ng Wei Qin
+10 Sept 2023
+"""
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class DynamicLabelsApp(App):
+    """Main program - Kivy app for dynamic label creation."""
