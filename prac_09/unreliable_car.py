f106efbc4796ea7be5d92b4ebe9ddb5376abdde6
feistyhippo <feistyhippo98@gmail.com>
2023-09-11 15:44:37 +0800

    Add drive method that calls Car's drive method but only drives if reliability > random number

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -5,13 +5,21 @@ Ng Wei Qin
 Unreliable car class
 """
 from car import Car
+import random
 
 
-class UnreliableCar:
+class UnreliableCar(Car):
     """Specialised version of a Car that drives based on a percentage chance."""
 
     def __init__(self, name, fuel, reliability):
         """Initialise an UnreliableCar."""
-        super.__init__(name, fuel)
+        super().__init__(name, fuel)
         self.reliability = reliability
 
+    def drive(self, distance, reliability):
+        """Drive like parent Car but only if reliability > random number."""
+        distance_driven = super().drive(distance)
+        if reliability > random.randint(0, 100):
+            return distance_driven
+        else:
+            return 0
