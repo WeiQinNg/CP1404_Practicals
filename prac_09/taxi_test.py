d4b3ac167dcceca9f85546737efa3f5f5a0785c7
feistyhippo <feistyhippo98@gmail.com>
2023-09-11 14:53:22 +0800

    Remove value for price_per_km

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -8,7 +8,7 @@ from taxi import Taxi
 
 
 def main():
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(f"{my_taxi}, current fare = ${my_taxi.get_fare()}")
     my_taxi.start_fare()
