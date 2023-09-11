fbe2df7887ba3984198d395a1441c5eee89f4b82
feistyhippo <feistyhippo98@gmail.com>
2023-09-11 14:44:18 +0800

    Add code to print my_taxi fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -10,10 +10,10 @@ from taxi import Taxi
 def main():
     my_taxi = Taxi("Prius 1", 100, 1.23)
     my_taxi.drive(40)
-    print(my_taxi)
+    print(f"{my_taxi}, current fare={my_taxi.get_fare()}")
     my_taxi.start_fare()
     my_taxi.drive(100)
-    print(my_taxi)
+    print(f"{my_taxi}, current fare={my_taxi.get_fare()}")
 
 
 main()
