5bf060f2e3a078d165fc19362d17c381a48b8566
feistyhippo <feistyhippo98@gmail.com>
2023-09-11 14:58:33 +0800

    Add unreliable car class

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -4,3 +4,10 @@ Ng Wei Qin
 11 Sept 2023
 Unreliable car class
 """
+from car import Car
+
+
+class UnreliableCar:
+    """Specialised version of a Car that drives based on a percentage chance."""
+
+    def __init__(self):
