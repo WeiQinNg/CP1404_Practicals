c53b85e3525348711daf3d464e78280bc92f721c
feistyhippo <feistyhippo98@gmail.com>
2023-09-16 11:19:13 +0800

    Compare two cars with different reliabilities and drive them for various distances

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -3,4 +3,23 @@ CP1404/CP5632 Practical 09
 Ng Wei Qin
 11 Sept 2023
 Unreliable car test
+Compare two cars with different reliabilities and drive them for various distances
 """
+
+from unreliable_car import UnreliableCar
+
+
+def main():
+    unreliable_car = UnreliableCar("Benz Patent Motorcar", 100, 25)
+    more_reliable_car = UnreliableCar("Toyota", 100, 80)
+
+    for i in range(1, 5):
+        print(f"Drive {i}km:")
+        print(f"{unreliable_car.name} drove {unreliable_car.drive(i)}km")
+        print(f"{more_reliable_car.name} drove {more_reliable_car.drive(i)}km")
+
+    print(unreliable_car)
+    print(more_reliable_car)
+
+
+main()
