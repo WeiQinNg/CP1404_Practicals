631dc858f1daf9b54b1282a6b771d981e980cf46
feistyhippo <feistyhippo98@gmail.com>
2023-09-16 11:18:17 +0800

    Edit drive method to evaluate distance first, then call parent Car's drive method to give distance driven

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -16,10 +16,9 @@ class UnreliableCar(Car):
         super().__init__(name, fuel)
         self.reliability = reliability
 
-    def drive(self, distance, reliability):
+    def drive(self, distance):
         """Drive like parent Car but only if reliability > random number."""
+        if self.reliability <= random.randint(0, 100):
+            distance = 0
         distance_driven = super().drive(distance)
-        if reliability > random.randint(0, 100):
-            return distance_driven
-        else:
-            return 0
+        return distance_driven
