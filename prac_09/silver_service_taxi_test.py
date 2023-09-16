b28d78c5b82f7ffb53ab6b73e8c46a390ad66841
feistyhippo <feistyhippo98@gmail.com>
2023-09-16 14:05:20 +0800

    Add program to test SilverServiceTaxi class

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,9 +4,16 @@ Ng Wei Qin
 16 Sept 2023
 SilverServiceTaxi test
 """
+from silver_service_taxi import SilverServiceTaxi
+
 
 def main():
-
+    old_taxi = SilverServiceTaxi("Old taxi", 100, 2)
+    fancy_taxi = SilverServiceTaxi("BMW", 100, 8)
+    old_taxi.drive(18)
+    fancy_taxi.drive(18)
+    print(f"{old_taxi}, current fare = ${old_taxi.get_fare()}")
+    print(f"{fancy_taxi}, current fare = ${fancy_taxi.get_fare()}")
 
 
 main()
