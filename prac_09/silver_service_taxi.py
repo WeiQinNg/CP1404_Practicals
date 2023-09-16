254a864a26881e86fb35bfe9f5d40b488f14aa13
feistyhippo <feistyhippo98@gmail.com>
2023-09-16 13:16:26 +0800

    Add SilverServiceTaxi class

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 Practical 09
+Ng Wei Qin
+16 Sept 2023
+SilverServiceTaxi class
+"""
+from taxi import Taxi
+
+
+class SilverServiceTaxi:
+    """Specialised version of a Car that includes fare costs based on fanciness."""
+
+    
