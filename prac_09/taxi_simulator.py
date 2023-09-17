5e251a5a7e8c53af95659a42ad302d1dc5cca2a4
feistyhippo <feistyhippo98@gmail.com>
2023-09-17 22:27:52 +0800

    Add taxi simulator program

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,14 @@
+"""
+CP1404/CP5632 Practical 09
+Ng Wei Qin
+17 Sept 2023
+Taxi simulator program
+"""
+from taxi import Taxi
+from silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+
+
+
