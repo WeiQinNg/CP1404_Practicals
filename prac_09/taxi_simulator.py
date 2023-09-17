c5637e4e0339ab57a2216f3529b4b39a245aa041
feistyhippo <feistyhippo98@gmail.com>
2023-09-17 23:03:57 +0800

    Add code segment for choosing a taxi

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -7,8 +7,27 @@ Taxi simulator program
 from taxi import Taxi
 from silver_service_taxi import SilverServiceTaxi
 
+MENU = "q)uit, c)hoose taxi, d)rive"
+
 
 def main():
+    print("Let's drive!")
+    menu_choice = input(">>> ").lower()
+    while menu_choice != "q":
+        total_bill = 0
+        taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+        current_taxi = None
+        if menu_choice == "c":
+            for number, taxi in enumerate(taxis):
+                print(f"{number} - {taxi}")
+            try:
+                index_choice = input("Choose taxi: ")
+                taxi_choice = taxis[index_choice]
+            except ValueError:
+                print("Invalid taxi choice")
+        elif menu_choice == "d":
 
 
 
+
+main()
