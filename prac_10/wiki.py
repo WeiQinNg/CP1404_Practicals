13cd204dfce0784db97fd725499a3f67d5e4dc05
feistyhippo <feistyhippo98@gmail.com>
2023-09-24 23:00:30 +0800

    Remove user input out of try

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -8,14 +8,16 @@ import wikipedia
 
 def main():
     title = " "
+    title = input("Title: ").title()
     while title != "":
         try:
-            title = input("Title: ").title()
             print(wikipedia.summary(title))
         except wikipedia.exceptions.DisambiguationError as e:
+            print("Please specify the title from the list:")
             print(e.options)
         except wikipedia.exceptions.PageError:
             print(f"{title} does not match any pages. Try another query!")
+        title = input("Title: ").title()
     print("End.")
 
 
