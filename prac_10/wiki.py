062c7f51a6d0e76f121aaadc4816ca0e83f8b764
feistyhippo <feistyhippo98@gmail.com>
2023-09-24 22:54:49 +0800

    Add exceptions for Disambiguation and Page errors

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -9,8 +9,13 @@ import wikipedia
 def main():
     title = " "
     while title != "":
-        title = input("Title: ")
-        print(wikipedia.summary(title))
+        try:
+            title = input("Title: ").title()
+            print(wikipedia.summary(title))
+        except wikipedia.exceptions.DisambiguationError as e:
+            print(e.options)
+        except wikipedia.exceptions.PageError:
+            print(f"{title} does not match any pages. Try another query!")
     print("End.")
 
 
